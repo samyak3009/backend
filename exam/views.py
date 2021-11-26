5d4b856261f2315dce1ad5e0a29b92d3efa8c2ff
samyak3009 <samyak3009@gmail.com>
2021-11-27 00:22:10 +0530

    final commit

--- exam/views.py
+++ exam/views.py
@@ -290,12 +290,18 @@ def test(request):
 			data = {'msge':'student inserted'}
 			status = statusCodes.STATUS_SUCCESS
 			return functions.RESPONSE(data,status)
-		elif(requestMethod.custom_request_type(request.GET, 'get_student')):
-			qry = list(StudentDetails.objects.filter().values('id','name','username','password'))
+		elif(requestMethod.custom_request_type(request.GET, 'update')):
+			# qry = StudentDetails.objects.filter(id=1).update(id=1001)
+			# qry = StudentDetails.objects.filter(id=2).update(id=1002)
+			qry = StudentDetails.objects.filter(id=3).update(id=1003)
+			data = {'data':'qry'}
+			status = statusCodes.STATUS_SUCCESS
+			return functions.RESPONSE(data,status)
+		elif(requestMethod.custom_request_type(request.GET, 'update')):
+			qry = list(StudentDetails.objects.filter().values('id','course','username','name','password'))
 			data = {'data':qry}
 			status = statusCodes.STATUS_SUCCESS
 			return functions.RESPONSE(data,status)
-
 		else:
 			data = statusMessages.MESSAGE_FORBIDDEN
 			status = statusCodes.STATUS_FORBIDDEN
