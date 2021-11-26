6857c91dab4534ff85254d280a691ca472160af3
samyak3009 <samyak3009@gmail.com>
2021-11-26 23:58:58 +0530

    final commit

--- exam/urls.py
+++ exam/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import create_form,login, submit_answer,check_status
+from .views import create_form,login, submit_answer,check_status, test
 urlpatterns=[
 	url(r'^create_form/$',create_form),
 	url(r'^login/$',login),
 	url(r'^submit_answer/$',submit_answer),
 	url(r'^check_status/$',check_status),
+	url(r'^test',test),
 ]
