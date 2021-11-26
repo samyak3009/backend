8d86bd4a1ecaf786bd93f8a3f134d97c497ccc47
samyak3009 <samyak3009@gmail.com>
2021-11-27 00:33:24 +0530

    final commit

--- exam/views.py
+++ exam/views.py
@@ -302,6 +302,19 @@ def test(request):
 			data = {'data':qry}
 			status = statusCodes.STATUS_SUCCESS
 			return functions.RESPONSE(data,status)
+		elif(requestMethod.custom_request_type(request.GET, 'clear_all_data')):
+			qry = ExamAnswer.objects.all().delete()
+			qry2 = ExamSession.objects.all().delete()
+			qry3 = ExamAttribute.objects.all().delete()
+			qry4 = ExamDetail.objects.all().delete()
+			data = {'data':'success cleared all data'}
+			status = statusCodes.STATUS_SUCCESS
+			return functions.RESPONSE(data,status)
+		elif(requestMethod.custom_request_type(request.GET, 'get_all_form')):
+			qry = list(ExamDetail.objects.filter().values('id','course','subject','status','start_date','end_date'))
+			data = {'data':qry}
+			status = statusCodes.STATUS_SUCCESS
+			return functions.RESPONSE(data,status)
 		else:
 			data = statusMessages.MESSAGE_FORBIDDEN
 			status = statusCodes.STATUS_FORBIDDEN
